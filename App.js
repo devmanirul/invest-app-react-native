abe4641f70735ffe620386ff5bd68790604cdd86
Manirul Islam <154756557+devmanirul@users.noreply.github.com>
2024-10-13 10:35:25 +0600

    HomePage comment

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="HomePage" component={HomePage} />
+        {/* <Stack.Screen name="HomePage" component={HomePage} /> */}
         <Stack.Screen name="SignUp" component={SignUp} />
         <Stack.Screen name="CreateAccount" component={CreateAccount} />
       </Stack.Navigator>
